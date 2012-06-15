d93b9c324cc64eedcf3c458a0eb895ac79968606
raj139 <bhagyatulasi139@gmail.com>
2012-06-15 21:04:08 +0530

    test

--- cookbooks/inmobi_lb_haproxy/recipes/querytags.rb
+++ cookbooks/inmobi_lb_haproxy/recipes/querytags.rb
@@ -1,5 +1,5 @@
 remote_recipe "remote recipe for lbchecks" do
   recipe "inmobi_lb_haproxy::runremote.rb"
-  attributes { from => @node[:rightscale][:instance_uuid] }
+  attributes { turns =< "10", from => @node[:rightscale][:instance_uuid] }
   recipients_tags ["loadbalancer:cms=lb"]
 end
