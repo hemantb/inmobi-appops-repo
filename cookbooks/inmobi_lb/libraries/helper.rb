0dd09c66dcb6c60d205ae5aa9b2a6083d8a58271
raj139 <bhagyatulasi139@gmail.com>
2012-06-16 16:49:24 +0530

    test

--- cookbooks/inmobi_lb/libraries/helper.rb
+++ cookbooks/inmobi_lb/libraries/helper.rb
@@ -31,6 +31,7 @@ module RightScale
       def query_appservers(vhost_name)
         app_servers = Hash.new
 
+=begin
         r=rightscale_server_collection 'app_servers' do
           tags ["loadbalancer:#{vhost_name}=app"]
           secondary_tags ["server:uuid=*", "appserver:listen_ip=*", "appserver:listen_port=*"]
@@ -38,6 +39,7 @@ module RightScale
         end
         r.run_action(:load)
 
+=end
         node[:server_collection]['app_servers'].to_hash.values.each do |tags|
           uuid = RightScale::Utils::Helper.get_tag_value('server:uuid', tags)
           ip = RightScale::Utils::Helper.get_tag_value('appserver:listen_ip', tags)
