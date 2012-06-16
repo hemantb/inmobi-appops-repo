177cbacba7c4d16430ad0d92c92aff5510bd429f
raj139 <bhagyatulasi139@gmail.com>
2012-06-16 17:11:49 +0530

    test

--- cookbooks/rightscale/providers/server_collection.rb
+++ cookbooks/rightscale/providers/server_collection.rb
@@ -5,13 +5,12 @@
 # RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
 # if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
-#require 'timeout'
+require 'timeout'
 
 # Provider collects and processes server tags used by the server_collection resource
 action :load do
 
 log "inside load and no issues"
-=begin
   collection_resource = server_collection new_resource.name do
     tags new_resource.tags
     agent_ids new_resource.agent_ids
@@ -29,6 +28,8 @@ log "inside load and no issues"
         collection_resource.run_action(:load)
         collection = node[:server_collection][new_resource.name]
 
+        break
+=begin
         break if new_resource.empty_ok && collection.empty?
         break if !collection.empty? && collection.all? do |id, tags|
           all_tags.all? do |prefix|
@@ -36,14 +37,14 @@ log "inside load and no issues"
           end
         end
 
-#        delay = calculate_exponential_backoff(delay)
-        Chef::Log.info "not all tags for #{new_resource.tags.inspect} exist; retrying in #{delay} seconds..."
-        sleep delay
+=end
+#        delay = ((delay == 1)? 2 : (delay*delay))
+#        Chef::Log.info "not all tags for #{new_resource.tags.inspect} exist; retrying in #{delay} seconds..."
+#        sleep delay
       end
     end
   rescue Timeout::Error => e
     raise "ERROR: timed out trying to find servers tagged with #{new_resource.tags.inspect}"
   end
 
-=end
 end
