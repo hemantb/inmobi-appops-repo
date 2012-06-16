ac0356a1b4f6046d72e1383685bf0b6cf6f3c13d
raj139 <bhagyatulasi139@gmail.com>
2012-06-16 18:32:26 +0530

    test

--- cookbooks/inmobi_lb/recipes/testing.rb
+++ cookbooks/inmobi_lb/recipes/testing.rb
@@ -1,19 +1,20 @@
 rightscale_marker :begin
 
-        main_tags = ["loadbalancer:rs1.app.ev1.inmobi.com=app"]
-        secondary_tags = ["server:uuid=*", "appserver:listen_ip=*", "appserver:listen_port=*"]
+main_tags = ["loadbalancer:rs1.app.ev1.inmobi.com=app"]
+secondary_tags = ["server:uuid=*", "appserver:listen_ip=*", "appserver:listen_port=*"]
 
-        r = server_collection 'app_servers' do
-          tags main_tags
-          action :nothing
-        end
+r = server_collection 'app_servers' do
+  tags main_tags
+   action :nothing
+end
 
-        r.run_action(:load)
+r.run_action(:load)
 
-	log "Printing tags"
-        node[:server_collection]["app_servers"].to_hash.values.each do |tags|
-		log "this is the tag #{tags}"
-	end
+log "Printing tags"
+
+node[:server_collection]["app_servers"].to_hash.values.each do |tags|
+     log "this is the tag #{tags}"
+end
 
 log "done printing tags"
 
