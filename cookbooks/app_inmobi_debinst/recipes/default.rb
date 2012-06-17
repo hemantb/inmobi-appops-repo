bb8111d1a4d27dc2e7401b79efc0f79bd2d84887
raj139 <bhagyatulasi139@gmail.com>
2012-06-17 23:05:47 +0530

    test

--- cookbooks/app_inmobi_debinst/recipes/default.rb
+++ cookbooks/app_inmobi_debinst/recipes/default.rb
@@ -7,9 +7,15 @@ log "service = #{node[:app_inmobi_debinst][:service]}"
 log "latest = #{node[:app_inmobi_debinst][:latest]}"
 
 execute "update apt cache" do
- command "apt-get update"
- ignore_failure true
+  command "apt-get update"
+  ignore_failure true
 end
+
+service "#{node[:app_inmobi_debinst][:service]}" do
+  start_command "#{node[:app_inmobi_debinst][:startcmd]}" unless node[:app_inmobi_debinst][:startcmd] == nil
+  stop_command "#{node[:app_inmobi_debinst][:stopcmd]}" unless node[:app_inmobi_debinst][:stopcmd] == nil
+  action :restart
+}
 
 node[:app_inmobi_tomcat][:webapp][:debians] .each do |p|
   log "Installing #{p}"
@@ -17,21 +23,16 @@ node[:app_inmobi_tomcat][:webapp][:debians] .each do |p|
      package $1 do
         version $2
         options "--force-yes"
-        notifies :restart , "app_inmobi_debinst[debinst]" unless node[:app_inmobi_debinst][:restart] == "false"
+        notifies :restart , "service[#{node[:app_inmobi_debinst][:startcmd]}]" unless node[:app_inmobi_debinst][:restart] == "false"
      end
    elsif node[:app_inmobi_tomcat][:webapp][:latest] == "true"
      package p do
         options "--force-yes"
-        notifies :restart , "app_inmobi_debinst[debinst]" unless node[:app_inmobi_debinst][:restart] == "false"
+        notifies :restart , "service[#{node[:app_inmobi_debinst][:startcmd]}]" unless node[:app_inmobi_debinst][:restart] == "false"
      end
    else
      raise "#{p} doesn't match the pattern packagename=version format. please fix or set latest? variable to true"
    end
-end
-
-app_inmobi_debinst "debinst" do
-  provider "app_inmobi_debinst"
-  action :nothing
 end
 
 right_link_tag "appserver:active=true"
