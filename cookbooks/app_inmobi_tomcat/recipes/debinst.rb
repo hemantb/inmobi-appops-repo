75df0e81f0fbb9e93618595076621295b89a53b5
raj139 <bhagyatulasi139@gmail.com>
2012-06-17 20:04:47 +0530

    test

--- cookbooks/app_inmobi_tomcat/recipes/debinst.rb
+++ cookbooks/app_inmobi_tomcat/recipes/debinst.rb
@@ -4,27 +4,27 @@ include_recipe "app_inmobi_tomcat::default"
 
 log "debians #{node[:app_inmobi_tomcat][:webapp][:debians]}"
 
+service "tomcat6" do
+  action :nothing
+  not_if {node[:app_inmobi_tomcat][:webapp][:restart] == "false"}
+end
+
 node[:app_inmobi_tomcat][:webapp][:debians] .each do |p|
   log "Installing #{p}"
   if p =~ /^([^=]+)=(.+)$/
      package $1 do
         version $2
         options "--force-yes"
+        notifies :restart , resources(:service => "tomcat6")
      end
    elsif node[:app_inmobi_tomcat][:webapp][:latest] == "true"
      package p do
-       options "--force-yes"
+        options "--force-yes"
+        notifies :restart , resources(:service => "tomcat6")
      end
    else
      raise "#{p} doesn't match the pattern packagename=version format. please fix or set latest? variable to true"
    end
-end
-
-case node[:app_inmobi_tomcat][:webapp][:restart]
-when "true"
-  app_inmobi_tomcat "default" do
-    action :restart
-  end
 end
 
 right_link_tag "appserver:active=true"
