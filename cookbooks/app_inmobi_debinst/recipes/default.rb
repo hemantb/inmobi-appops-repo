a30a1f2d88d67498b6c0a8951d3640b07fe13970
raj139 <bhagyatulasi139@gmail.com>
2012-06-17 21:51:02 +0530

    test

--- cookbooks/app_inmobi_debinst/recipes/default.rb
+++ cookbooks/app_inmobi_debinst/recipes/default.rb
@@ -4,6 +4,17 @@ app_inmobi_debinst "app_inmobi_debinst" do
   action :nothing
 end
 
+log "restart = node[:app_inmobi_debinst][:restart]"
+log "start = node[:app_inmobi_debinst][:stopcmd]"
+log "stop = node[:app_inmobi_debinst][:startcmd]"
+log "service = node[:app_inmobi_debinst][:service]"
+log "latest = node[:app_inmobi_debinst][:latest]"
+
+execute "update apt cache" do
+ command "apt-get update"
+ ignore_failure true
+end
+
 node[:app_inmobi_tomcat][:webapp][:debians] .each do |p|
   log "Installing #{p}"
   if p =~ /^([^=]+)=(.+)$/
