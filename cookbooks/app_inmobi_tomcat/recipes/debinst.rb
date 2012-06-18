e58c1d9e1c1a964b10504de71e49f0e1c23c63ee
raj139 <bhagyatulasi139@gmail.com>
2012-06-18 06:50:24 +0530

    test

--- cookbooks/app_inmobi_tomcat/recipes/debinst.rb
+++ cookbooks/app_inmobi_tomcat/recipes/debinst.rb
@@ -9,7 +9,8 @@ service "tomcat6" do
   action :nothing
 end
 
-node[:app_inmobi_tomcat][:webapp][:debians] .each do |p|
+debians = node[:app_inmobi_tomcat][:webapp][:debians]
+debians.gsub(/\s+/, "").split(",").uniq.each do |p|
   log "Installing #{p}"
   if p =~ /^([^=]+)=(.+)$/
      package $1 do
