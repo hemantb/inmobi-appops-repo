91ff169b7b9b1bf5d101a605d7b2e10ce676f339
raj139 <bhagyatulasi139@gmail.com>
2012-06-20 21:16:14 +0530

    test

--- cookbooks/app_inmobi_lb/templates/default/haproxy-genconf.pl
+++ cookbooks/app_inmobi_lb/templates/default/haproxy-genconf.pl
@@ -12,15 +12,15 @@ close FILE;
 foreach my $list (@dirlist) {
 	chomp $list;
 	if (-d "$CONF_DIR/lb_haproxy.d/$list" && -f "$CONF_DIR/lb_haproxy.d/${list}.cfg") {
-		print "pushing $list\n";
 		my $count = `ls -1 $CONF_DIR/lb_haproxy.d/$list | wc -l`;
 		chomp $count; next if ! $count;
 		$list =~ s/\s+$//;
 		$list =~ s/^\s+//;
-		if ($list =~ /^(sticky|notsticky|nosticky)-(\d+)-(.+)-(\d+)$/) {
-			$VHOSTS{$2}{$list}{sticky} = $1;
-			$VHOSTS{$2}{$list}{dest} = $4;
-			$VHOSTS{$2}{$list}{vhost} = $3;
+		if ($list =~ /^(sticky|notsticky|nosticky)-(http|tcp)-(\d+)-(.+)-(\d+)$/) {
+			$VHOSTS{$3}{$list}{sticky} = $1;
+			$VHOSTS{$3}{$list}{mode} = $2;
+			$VHOSTS{$3}{$list}{vhost} = $4;
+			$VHOSTS{$3}{$list}{dest} = $5;
 		} else {
 			print STDERR "$list is not in valid format\n";
 		}
@@ -28,18 +28,32 @@ foreach my $list (@dirlist) {
 }
 
 foreach my $fport (keys %VHOSTS) {
-	$FRONTEND .= "frontend ${fport}_requests *:$fport\n\n";
+
 	foreach my $vhost (keys %{$VHOSTS{$fport}}) {
 		my ($acl, $backend);
-		$acl = $backend = $vhost;
-		$acl =~ s/\./_/g;
-		$backend =~ s/\./_/g;
-		$FRONTEND .= "        acl ${acl}_acl hdr_dom(host) -i $VHOSTS{$fport}{$vhost}{vhost}\n";
-		$FRONTEND .= "        use_backend ${backend}_backend if ${acl}_acl\n\n";
 
+		$LISTEN .= "listen $vhost *:$fport\n\n";
+		$LISTEN .= "balance roundrobin\n";
+		$LISTEN .= "mode $VHOSTS{$fport}{$vhost}{mode}\n\n";
+		$LISTEN .= "cookie SERVERID insert indirect nocache\n\n";
+		$LISTEN .= "appsession JSESSIONID len 100 timeout 1w\n" if $VHOSTS{$fport}{$vhost}{sticky} eq 'sticky';
+                 
 		open (FILE, "$CONF_DIR/lb_haproxy.d/${vhost}.cfg") || die "Can't open config file $CONF_DIR/lb_haproxy.d/${vhost}.cfg";
-		$BACKEND .= join("", <FILE>);
+
+                foreach my $line (<FILE>) {
+			$line =~ s/\s+$//;
+			$line =~ s/^\s+//;
+			next if $line !~ /^([^=]+)=([^=]+)$/;
+                        $listencont{$1} = $2;
+                }
 		close FILE;
+
+                $LISTEN .= "$listencont{status_uri}\n" if $listencont{status_uri};
+                $LISTEN .= "$listencont{health_uri}\n" if $listencont{health_uri};
+                $LISTEN .= "$listencont{health_check_line}\n" if $listencont{health_check_line} and $listencont{health_uri};
+                $ILSTEN .= "\n\n";
+
+		$LISTEN .= "        server disabled-server 127.0.0.1:1 check downinter 2000d\n";
 
 		my @hostlist = `find $CONF_DIR/lb_haproxy.d/$vhost -type f`;
 		foreach my $bhost (@hostlist) {
@@ -52,12 +66,12 @@ foreach my $fport (keys %VHOSTS) {
 				$contents{$1} = $2;
 			}
 			close FILE;
-			$BACKEND .= "        server $contents{server} $contents{backend_ip}:$VHOSTS{$fport}{$vhost}{dest} ";
-			$BACKEND .= "cookie $contents{server} " if $contents{stickyness} eq 'sticky';
-			$BACKEND .= "check " if $contents{health_check_uri};
-			$BACKEND .= "$contents{extraopts} " if $contents{extraopts};
-			$BACKEND .= "maxconn $contents{maxconn}" if $contents{maxconn};
-			$BACKEND .= "\n\n"
+			$LISTEN .= "        server $contents{server} $contents{backend_ip}:$VHOSTS{$fport}{$vhost}{dest} ";
+			$LISTEN .= "cookie $contents{server} ";
+			$LISTEN .= "check " if $contents{health_check_uri};
+			$LISTEN .= "$contents{extraopts} " if $contents{extraopts};
+			$LISTEN .= "maxconn $contents{maxconn}" if $contents{maxconn};
+			$LISTEN .= "\n\n";
 		}
 	}
 }
@@ -65,7 +79,10 @@ foreach my $fport (keys %VHOSTS) {
 open(FILE, ">$CONF_FILE") || die "Couldnt write to config file $CONF_FILE";
 
 print FILE $HEAD;
-print FILE $FRONTEND;
-print FILE $BACKEND;
+print FILE $LISTEN;
+
+print $HEAD;
+print $LISTEN;
 
 close FILE;
+
